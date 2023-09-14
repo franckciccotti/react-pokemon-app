f5f602ba4fe11fd393ef20feb7b3fe5c5ee48ca7
Franck CICCOTTI <franckciccotti@me.com>
2023-09-15 00:23:08 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+  
+const App: FunctionComponent = () => {
+ const name: String = 'React';
+    
+ return (
+  <h1>Bonjour, {name} !</h1>
+ )
+}
+  
+export default App;
