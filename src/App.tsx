35feadc6f8f70be7a699e2dab0e2ad21c78a2953
Franck CICCOTTI <franckciccotti@me.com>
2023-09-18 21:43:49 +0200

    les routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent } from 'react';
+
 import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import PageNotFound from './pages/page-not-found';
+
+import { BrowserRouter as Link, Route, Router, Switch } from 'react-router-dom';
   
 const App: FunctionComponent = () => {
     
-    return(
-        <PokemonList/>
+    return (
+        <Router>
+            <div>
+
+                {/* La barre de navigation commun à toutes les pages */}
+                <nav>
+                    <div className='nav-wrapper teal'>
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+
+                {/* Le système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+                    <Route component={PageNotFound} />
+                </Switch>
+
+            </div>
+        </Router>
     )
 }
   
