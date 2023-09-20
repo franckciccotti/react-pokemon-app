3b59b48c64ed4172a41e9b7b7734d532c16d2e2f
Franck CICCOTTI <franckciccotti@me.com>
2023-09-20 18:59:13 +0200

    formulaire de modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-
 import PokemonList from './pages/pokemon-list';
 import PokemonsDetail from './pages/pokemon-detail';
 import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 
 const App: FunctionComponent = () => {
     
@@ -23,6 +23,7 @@ const App: FunctionComponent = () => {
                 <Switch>
                     <Route exact path="/" component={PokemonList} />
                     <Route exact path="/pokemons" component={PokemonList} />
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
                     <Route exact path="/pokemons/:id" component={PokemonsDetail} />
                     <Route component={PageNotFound} />
                 </Switch>
